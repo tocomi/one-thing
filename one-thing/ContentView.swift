d2713a19a6a1c47ede017ea9e0bc72f4d9f00c33
tocomi <tocomi0112@gmail.com>
2026-05-05 13:53:06 +0900

    Initial Commit

---
+++ one-thing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  one-thing
+//
+//  Created by Kenta TSUNEMI on 2026/05/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ one-thing/one_thingApp.swift
@@ -0,0 +1,17 @@
+//
+//  one_thingApp.swift
+//  one-thing
+//
+//  Created by Kenta TSUNEMI on 2026/05/05.
+//
+
+import SwiftUI
+
+@main
+struct one_thingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
